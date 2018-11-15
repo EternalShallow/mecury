6150717ea6e80fbba94e010e9e0ddef18706414f
zhangdongfei@lushangyouwo.com <zhangdongfei@lushangyouwo.com>
2018-11-15 14:27:20 +0800

    borrowBooks押金显示NaN的修改

--- xiaobaoPC/src/views/mainline/book/component/header/index.vue
+++ xiaobaoPC/src/views/mainline/book/component/header/index.vue
@@ -18,7 +18,7 @@
           <div>借书情况：<label>{{ data.borrowBooksCount }}</label></div>
           <div>借书次数：<label>{{ data.borrowCount }}</label></div>
           <div>截止日期：<label>{{ data.borrowDeadline }}</label></div>
-          <div class="red">押金：<label>{{ data.depositMoney / 100 }}</label></div>
+          <div class="red">押金：<label>{{ data.hasOwnProperty('depositMoney') ? data.depositMoney / 100 : '' }}</label></div>
           <div>可借阅多少本：<label>{{ data.maxBorrowCount }}</label></div>
           <div>状态：<label>{{ data.status }}</label></div>
           <div>入场券截止日期：<label>{{ data.ticketDeadline }}</label></div>
